2f0f189e67a0b71d20bb29f8b2e0dabd3fccf87f
Wyatt Arnold <wyatt.arnold@water.ca.gov>
2020-09-17 13:28:34 -0700

    COSVF evolutionary search
    COSVF evolutionary search
    
    all data inputs and code necessary for running evolutionary search for carryover storage value functions on surface and groundwater reservoirs over the full CALVIN network.
    
    Revise GW reservoirs included in penalty search
    
    CALVIN GW reservoirs 03, 04, 06, 07, 09, 14, 15, 19, and IM were removed from the rtype2  COSVF penalty search. These reservoirs do not exhibit any overdraft condition in a limited foresight myopic run and have a final dual value of zero in the perfect foresight groundwater overdraft constrained run.
    
    fixes and slight adjustments
    
    - default directory fix
    - random seed selection
    - operations costs were missing links due to faulty dataframe filter
    - initial random parameter values on COSVF are applied uniformly for both rtype1 and 2. This allows handling of an odd number of linear (rtype2) COSVF reservoirs.
    
    csv writer
    
    encoding and new line marker
    
    Create operation_groups.csv
    
    used for post-process grouping of variable operation costs
    
    Update main-cosvf.py
    
    collect objective fitness values
    
    cleanup and checkpointing for DEAP
    
    - remove water year flag
    - drop persuasion penalties from cost calc
    - remove initial storage setting (redundant)
    - date handling in variable constraints
    - checkpointing with DEAP
    - adjust initial individual COSVF bounds
    - remove unused Pareto code
    
    Update main-cosvfea.py
    
    change defaults
    
    Delete cosvf-data-prep.py

---
+++ main-cosvf.py
@@ -0,0 +1,24 @@
+import os, shutil
+
+from calvin import cosvfea
+
+# CONFIG
+COSVF_DIR = './my-models/calvin-lf'     # DIRECTORY FOR LINKS, INFLOWS, SR_DICT etc.
+SOLVER, NPROC = 'cbc', 1             # SOLVER AND CPU
+
+# construct results path from run name
+result_dir = os.path.join(COSVF_DIR,'results')
+if not os.path.isdir(result_dir): os.makedirs(result_dir)
+
+# calvin instance
+calvin = cosvfea.COSVF(pwd=COSVF_DIR)
+calvin.create_pyomo_model(debug_mode=True)
+
+# solve model
+f1, f2, f3 = calvin.cosvf_solve(solver=SOLVER, nproc=NPROC, resultdir=result_dir)
+print('total cost={} $M/yr'.format(f1))
+print('gw overdraft ={} MAF/yr'.format(f2))
+
+# copy the COSVF params used to the results directory
+shutil.copy(os.path.join(COSVF_DIR,'cosvf-params.csv'), 
+            os.path.join(result_dir,'cosvf-params.csv'))
